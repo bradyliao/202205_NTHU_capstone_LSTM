8b4f1484cf22bfde5ddf6cea7aef78aea8761094
Brady <bradyliao123@gmail.com>
2022-05-12 00:49:55 +0800

    organize the code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # https://ithelp.ithome.com.tw/articles/10206312
 
-num_of_epochs = 1
+num_of_epochs = 40
 num_of_batch_size = 32
-num_of_timesteps = 60
-num_of_features = 4
+timesteps = 60
+days_forward = 0 # predicting how many days after, 0 being the immediate next
+features = ['Open','High', 'Low', 'Close']
+num_of_features = len(features)
 
 # ----------------------------------------------------------------------------------------
 # Import the libraries
@@ -13,18 +15,26 @@ import pandas as pd
 
 # ----------------------------------------------------------------------------------------
 # Import the dataset
-dataset = pd.read_csv('googl.us.csv')
+dataset_in = pd.read_csv('googl.us.csv')
 
+# extract features
+dataset = dataset_in[features]
+
+# calculate total num of data
 total_num_data = len(dataset)
-total_num_training = int(total_num_data *2/3)
-total_num_testing = int(total_num_data *1/3)
+total_num_training = int(total_num_data * 0.9)
+total_num_testing = int(total_num_data * 0.1)
 
-# original
-# training_set = dataset.iloc[:total_num_training, 1:2].values  # 取「Open」欄位值
 
-# testing now
-training_set = dataset.iloc[:total_num_training, 1:5].values  # 取「Open」欄位值
+# training set
+training_set = dataset.iloc[:total_num_training].values
 assert num_of_features == training_set.shape[1]
+
+# testing set
+testing_set = dataset[total_num_training-timesteps:].values
+assert num_of_features == testing_set.shape[1]
+
+
 
 # ----------------------------------------------------------------------------------------
 # Feature Scaling
@@ -37,16 +47,40 @@ sc1 = MinMaxScaler(feature_range = (0, 1))
 temp = sc1.fit_transform(training_set[:,0:1])
 
 
-X_train = []   #預測點的前 60 天的資料
+testing_set = testing_set.reshape(-1, testing_set.shape[1])
+testing_set_scaled = sc.transform(testing_set) # Feature Scaling
+
+
+# ----------------------------------------------------------------------------------------
+# generate batch
+X_train = []   #預測點的前 timesteps 天的資料
 y_train = []   #預測點
-for i in range(60, total_num_training):
-    X_train.append( training_set_scaled [i-60:i, 0:num_of_features] ) # data of features
+for i in range(timesteps, total_num_training):
+    X_train.append( training_set_scaled [i-timesteps:i, 0:num_of_features] ) # data of features
     y_train.append( training_set_scaled [i, 0] ) # data of the target value
 X_train, y_train = np.array(X_train), np.array(y_train)  # 轉成numpy array的格式，以利輸入 RNN
-print(y_train.shape)
+#print(y_train.shape)
 
 X_train = np.reshape( X_train, ( X_train.shape[0], X_train.shape[1], X_train.shape[2] ) )
 assert num_of_features == X_train.shape[2]
+
+
+
+
+
+X_test = []
+for i in range(timesteps, timesteps + total_num_testing):  
+    X_test.append(testing_set_scaled[i-timesteps:i, 0:num_of_features])
+X_test = np.array(X_test)
+assert num_of_features == X_test.shape[2]
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], X_test.shape[2]))  # Reshape 成 3-dimension
+
+
+
+
+
+
+
 
 
 
@@ -61,65 +95,71 @@ from keras.layers import LSTM
 from keras.layers import Dropout
 
 # Initialising the RNN
-regressor = Sequential()
+model = Sequential()
 
 # Adding the first LSTM layer and some Dropout regularisation
 assert num_of_features == X_train.shape[2]
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], X_train.shape[2])))
-regressor.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], X_train.shape[2])))
+model.add(Dropout(0.2))
 
 # Adding a second LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
 
 # Adding a third LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50, return_sequences = True))
-regressor.add(Dropout(0.2))
+model.add(LSTM(units = 50, return_sequences = True))
+model.add(Dropout(0.2))
 
 # Adding a fourth LSTM layer and some Dropout regularisation
-regressor.add(LSTM(units = 50))
-regressor.add(Dropout(0.2))
+model.add(LSTM(units = 50))
+model.add(Dropout(0.2))
 
 # Adding the output layer
-regressor.add(Dense(units = 1))
+model.add(Dense(units = 1))
 
 # Compiling
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=["acc"])
 
 
 # ----------------------------------------------------------------------------------------
 # run model
-regressor.fit(X_train, y_train, epochs = num_of_epochs, batch_size = num_of_batch_size)
+history = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, epochs = num_of_epochs, batch_size = num_of_batch_size)
+
+
+
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+
+
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
 
 
 
 
 
 
+# ----------------------------------------------------------------------------------------
 
 
 
-
-
-
-testing_set = dataset[['Open','High', 'Low', 'Close']][total_num_training-60:].values
-assert num_of_features == testing_set.shape[1]
-testing_set = testing_set.reshape(-1, testing_set.shape[1])
-testing_set = sc.transform(testing_set) # Feature Scaling
-
-X_test = []
-for i in range(60, 60 + total_num_testing):  # timesteps一樣60
-    X_test.append(testing_set[i-60:i, 0:num_of_features])
-X_test = np.array(X_test)
-assert num_of_features == X_test.shape[2]
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], X_test.shape[2]))  # Reshape 成 3-dimension
-
-
-
-predicted_stock_price = regressor.predict(X_test)
-print(predicted_stock_price)
-print(predicted_stock_price.shape)
+predicted_stock_price = model.predict(X_test)
 predicted_stock_price = sc1.inverse_transform(predicted_stock_price)  # to get the original scale
+
 
 
 
@@ -130,7 +170,11 @@ plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock 
 plt.title('Google Stock Price Prediction')
 plt.xlabel('Time')
 plt.ylabel('Google Stock Price')
-configuration = "Epochs: " + str(num_of_epochs) + " , Batch size: " + str(num_of_batch_size) + " , Timesteps: " + str(num_of_timesteps)
+configuration = "Epochs: " + str(num_of_epochs) + " , Batch size: " + str(num_of_batch_size) + " , Timesteps: " + str(timesteps)
 plt.figtext(0.6, 0.02, configuration, wrap = True, fontsize=12)
 plt.legend()
 plt.show()
+
+
+
+
